490124b8117a18bd91b2ea9b8b69ded7b19e3c9e
mocanul <bdinulescu85@gmail.com>
2025-10-20 15:47:45 +0100

    sign up dev

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,4 @@
 // This is the homepage component of our project (http://localhost:3000)
 // Because it's in the "app" folder, Next.js automatically maps this file to the "/" route.
 
-import Navbar from "./components/navbar";
-
-export default function HomePage() {
-  return (
-
-    <div className="bg-white">
-
-
-
-
-      <Navbar />
-    </div>
-    );
-};
+export default function HomePage() {}
